7fd71de60fe6131d540f43add428cf89ce08363d
drew-loukusa <dlwerd@gmail.com>
2019-07-12 22:20:22 -0700

    Switched out outdated method call. Example code now working, now to add features

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        #await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
 
 @client.event
 async def on_ready():
